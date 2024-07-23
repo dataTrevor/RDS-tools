d4db440f8afbf76460d3edfb5478aa52f0621d61
Chuan Jin <chuanjin@amazon.com>
2024-07-23 20:49:46 +0800

    add DMS_CDC

--- replication_from_green_to_oldBlue.py
+++ replication_from_green_to_oldBlue.py
@@ -17,6 +17,9 @@ clusterRegion = os.environ['REGION']
 repl_user = os.environ['REPL_USER']
 repl_pass = os.environ['REPL_PASS']
 binlog_msg = os.environ['BINLOG_MSG']
+#now, cdc_method support 2 values: MY_REPLICATION, DMS_CDC
+cdc_method = os.environ['CDC_METHOD']
+repInstanceArn = os.environ['REP_INSTANCE_ARN']
 
 start_slave_sql = 'CALL mysql.rds_start_replication'
 logger = logging.getLogger()
@@ -32,10 +35,10 @@ def getConnection(rds_host, user_name, password, db_name=None):
         logger.error(e)
         sys.exit(1)
 
-def getRdsClient(srvName, region_name):
-    client = boto3.client('rds', region_name)
+def getClient(srvName, region_name):
+    client = boto3.client(srvName, region_name)
     return client
-    
+
 # get Aurora Instance list from AWS SDK by clusterID, not for RDS instance!
 def getCluster(client, clusterId):
     instanceList = []
@@ -94,6 +97,157 @@ def getBinlogFilePos(binlogMsg: str):
     binlogPos = binlogMsg[posStartInx:]
     return binlogFile.strip(), int(binlogPos)
 
+# create the MySQLSetting in DMS endpoint
+def MySQLSettingsJson(servername, port, username, password):
+    settingJson = {'ServerTimezone': 'UTC'}
+    settingJson['ServerName'] = servername
+    settingJson['Port'] = port
+    settingJson['Username'] = username
+    settingJson['Password'] = password
+
+    return settingJson
+
+# init the tableMappingJson for CDC task
+def initTableMappingJson():
+    tableMappingJson = {}
+    allSchemaRule = {}
+    allSchemaRule['rule-type'] = "selection"
+    allSchemaRule['rule-id'] = "395145838"
+    allSchemaRule['rule-name'] = "lambda-create-395145838"
+    ruleObject = {
+            "schema-name": "%",
+            "table-name": "%"
+    }
+    allSchemaRule['object-locator'] = ruleObject
+    allSchemaRule['rule-action'] = "include"
+    allSchemaRule['filters'] = []
+    allSchemaRule['parallel-load'] = None
+    allSchemaRule['isAutoSegmentationChecked'] = False
+    rules = [allSchemaRule]
+    tableMappingJson["rules"] = rules
+    return tableMappingJson
+
+# init task setting
+def initReplicationTaskSettings():
+    settingJson = {}
+    errorBehavior = {}
+    logging = {
+        "EnableLogging": True,
+        "CloudWatchLogGroup": None,
+        "CloudWatchLogStream": None
+        }
+    errorBehavior['FailOnNoTablesCaptured'] = False
+    settingJson['ErrorBehavior'] = errorBehavior
+    settingJson['Logging'] = logging
+    return settingJson
+
+# test DMS connection to endpoint
+def testDMSConnection(endpointArn, repInstanceArn, region):
+    try:
+        client = getClient('dms', region)
+        res = client.test_connection(
+                ReplicationInstanceArn=repInstanceArn,
+                EndpointArn=endpointArn
+        )
+        if res['Connection']['Status'] == "testing":
+            time.sleep(3)
+            logger.warn("%s is testing", endpointArn)
+            return testDMSConnection(endpointArn, repInstanceArn, region)
+        else:
+            return res
+    except client.exceptions.InvalidResourceStateFault as ExState:
+        logger.warn("%s test exception", endpointArn)
+        logger.warn(ExState)
+        time.sleep(6)
+        return testDMSConnection(endpointArn, repInstanceArn, region)
+    except Exception as e:
+        logger.error(e)
+        return None
+
+# check cdc task status
+def checkDMSCDCtaskStat(taskArn, region) -> str:
+    client = getClient("dms", region)
+    res = client.describe_replication_tasks(
+    Filters=[
+        {
+            'Name': 'replication-task-arn',
+            'Values': [
+                taskArn,
+            ]
+        },
+    ]
+    )
+    if res == None or len(res['ReplicationTasks'] == 0):
+        logger.warn("Task not found: %s", taskArn)
+        return None
+    else:
+    # get first task status
+        status = res['ReplicationTasks'][0]['Status']
+        # doing sth but not running replication
+        if status[-3:] == "ing" and status!="running":
+            return checkDMSCDCtaskStat(taskArn, region)
+        else:
+            return status
+
+# valid cdc task init status equal to "Created"
+def DMStaskStatInCreated(taskArn, region):
+    client = getClient("dms", region)
+    res = client.describe_replication_tasks(
+    Filters=[
+        {
+            'Name': 'replication-task-arn',
+            'Values': [
+                taskArn,
+            ]
+        },
+    ]
+    )
+    if res == None or len(res['ReplicationTasks'] == 0):
+        logger.warn("Task not found: %s", taskArn)
+        return False
+    else:
+    # get first task status
+        status = res['ReplicationTasks'][0]['Status']
+        # doing sth but not running replication
+        if status == "creating" or status[-3:]=="ing":
+            time.sleep(10)
+            return DMStaskStatInCreated(taskArn, region)
+        elif status == "ready" or status == "created":
+            return True
+        else:
+            return False
+
+#  delete blue green and confirm read only of Aurora2 =OFF
+def deleteBlueGreenDeploymentAndConfirm(client, deploymentIdentifier: str, blueclusterEndpoint: str, user_name, password):
+    deployment = deleteBlueGreenDeployment(client, deploymentIdentifier)
+    logging.warn("bule green deployment %s status: %s" % (deploymentIdentifier, deployment['Status']))
+    time.sleep(10)
+    readOnly = 'ON'
+    conn = getConnection(blueclusterEndpoint, user_name, password)
+    if conn is not None:
+        logger.info("SUCCESS: Connection to RDS for Aurora2 instance succeeded")
+    else:
+        return readOnly
+    # wait mysql5.7 to disable read only
+    for i in range(100):
+        time.sleep(3)
+        cur = conn.cursor()
+        # check if read_only = OFF
+        cur.execute("show variables like 'read_only'")
+        for row in cur:
+            varName = row[0]
+            readOnly = row[1]
+        if readOnly == 'ON':
+            logger.warn("The status of read_only is ON, to next loop")
+            cur.close()
+            continue
+        # read_only changed to OFF, begin config rollback replication from Aurora3(src) to Aurora2(dest)
+        elif readOnly == 'OFF':
+            cur.close()
+            break
+    conn.close()
+    return readOnly
+
 def lambda_handler(event, context):
     """
     This function creates a new RDS database table and writes records to it
@@ -111,7 +265,7 @@ def lambda_handler(event, context):
         logging.info("Not Switchover completed on blue/green deployment. EventID: %s, msg: %s" % (eventID, msg))
         return "Not Switchover completed on blue/green deployment. EventID: %s, msg: %s" % (eventID, msg) 
     
-    client = getRdsClient('rds', region)
+    client = getClient('rds', region)
     blueGreenDeployment = descBlueGreenDeployment(client, deploymentIdentifier)
     SourceArn = blueGreenDeployment['Source']
     TargetArn = blueGreenDeployment['Target']
@@ -119,6 +273,7 @@ def lambda_handler(event, context):
     #Status = SwitchoverDetail['Status']
     bluecluster = getCluster(client, SourceArn)
     logging.warn(bluecluster)
+    source_rds_port = bluecluster['Port']
     blueclusterEndpoint = bluecluster['Endpoint']
     blueDBClusterIdentifier = bluecluster['DBClusterIdentifier']
     greencluster = getCluster(client, TargetArn)
@@ -170,31 +325,19 @@ def lambda_handler(event, context):
     logging.info("Instance %s 's binlogMsg: %s" % (eventInstanceId, binlogMsg))
     master_binlog_file, master_binlog_pos = getBinlogFilePos(binlogMsg)
     logging.warn("binlog: %s, %d" % (master_binlog_file, master_binlog_pos))
-    # delete blue green deployment 
-    deployment = deleteBlueGreenDeployment(client, deploymentIdentifier)
-    logging.warn("bule green deployment %s status: %s" % (deploymentIdentifier, deployment['Status']))
-    time.sleep(10)
+    
     # then config replication for rollback
-    config_master_sql_string = f"CALL mysql.rds_set_external_master('{greenclusterEndpoint}', {source_rds_port}, '{repl_user}', '{repl_pass}', '{master_binlog_file}', {master_binlog_pos}, false)"
-    logger.info("config_master_sql_string: %s" % config_master_sql_string)
-    conn = getConnection(blueclusterEndpoint, user_name, password)
-    if conn is not None:
-        logger.info("SUCCESS: Connection to RDS for MySQL instance succeeded")
-    # wait mysql5.7 to disable read only
-    for i in range(100):
-        time.sleep(3)
-        cur = conn.cursor()
-        # check if read_only = OFF
-        cur.execute("show variables like 'read_only'")
-        varValue = 'ON'
-        for row in cur:
-            varName = row[0]
-            varValue = row[1]
-        if varValue == 'ON':
-            logger.warn("The status of read_only is ON, to next loop")
-            cur.close()
-            continue
-        elif varValue == 'OFF':
+    if cdc_method == 'MY_REPLICATION':
+        config_master_sql_string = f"CALL mysql.rds_set_external_master('{greenclusterEndpoint}', {source_rds_port}, '{repl_user}', '{repl_pass}', '{master_binlog_file}', {master_binlog_pos}, false)"
+        logger.info("config_master_sql_string: %s" % config_master_sql_string)
+        # delete blue green deployment 
+        readOnly = deleteBlueGreenDeploymentAndConfirm(client, deploymentIdentifier, blueclusterEndpoint, user_name, password)
+        if readOnly == "ON":
+            logger.error("Aurora2: %s can't write, pls check", blueDBClusterIdentifier)
+            return
+        elif readOnly == 'OFF':
+            conn = getConnection(blueclusterEndpoint, user_name, password)
+            cur = cur = conn.cursor()
             cur.execute(config_master_sql_string)
             cur.execute(start_slave_sql)
             # wait for replication connecting
@@ -203,7 +346,91 @@ def lambda_handler(event, context):
             logger.info("The status of slave replication is as following:")
             for row in cur:
                 logger.info(row)
-            break
+                break
+    elif cdc_method == 'DMS_CDC':
+        logger.info("You choose dms for cdc task")
+        logger.info("Begin to config dms cdc task")
+        # create DMS task with exists DMS replication instance
+        dmsclient = getClient('dms', region)
+        srcEndpointJson = MySQLSettingsJson(greenclusterEndpoint, source_rds_port, repl_user, repl_pass)
+        destEndpointJson = MySQLSettingsJson(blueclusterEndpoint, source_rds_port, user_name, password)
+        # create endpoint
+        try:
+            srcResponse = dmsclient.create_endpoint(EndpointIdentifier='src-green-' + greenDBClusterIdentifier,
+                EndpointType='source',
+                EngineName='aurora',
+                SslMode='none',
+                MySQLSettings=srcEndpointJson)
+            srcEndpointArn = srcResponse['Endpoint']['EndpointArn']
+            logger.info("Source Endpoint created successfully: %s", srcEndpointArn)
+            destResponse = dmsclient.create_endpoint(EndpointIdentifier='dest-blue-' + blueDBClusterIdentifier,
+                EndpointType='target',
+                EngineName='aurora',
+                SslMode='none',
+                MySQLSettings=destEndpointJson)
+            destEndpointArn = destResponse['Endpoint']['EndpointArn']
+            logger.info("Target Endpoint created successfully: %s", destEndpointArn)
+        except dmsclient.exceptions.ResourceAlreadyExistsFault as ex:
+            logger.warn("Endpoint exists in DMS, pls check as following")
+            logger.error(ex)
+            return
+        # test the connection of endpoints, no test
+        #testSrcResponse = testDMSConnection(srcEndpointArn, repInstanceArn, region)
+        #testDestResponse = testDMSConnection(destEndpointArn, repInstanceArn, region)
+        #"successful","testing","failed","deleting"
+        #if testSrcResponse['Connection']['Status'] == 'successful' and testDestResponse['Connection']['Status'] == 'successful':
+        if 'successful' == 'successful':
+            # create cdc task
+            try:
+                logger.info("Begin to create cdc task")
+                tableMappingJson = initTableMappingJson()
+                cdcResponse = dmsclient.create_replication_task(ReplicationTaskIdentifier='cdc-after-bg-switch-'+ clusterId,
+                    SourceEndpointArn=srcEndpointArn,
+                    TargetEndpointArn=destEndpointArn,
+                    ReplicationInstanceArn=repInstanceArn,
+                    MigrationType='cdc',
+                    CdcStartPosition="%s:%d" % (master_binlog_file, master_binlog_pos),
+                    TableMappings=json.dumps(tableMappingJson),
+                    ReplicationTaskSettings=json.dumps(initReplicationTaskSettings())
+                )
+                cdcArn = cdcResponse['ReplicationTask']['ReplicationTaskArn'] if cdcResponse != None else None
+                if cdcArn is None:
+                    logger.error("CDC task of %s creatd failed after blue/green switch", clusterId)
+                    return
+                # delete blue green
+                readOnly = deleteBlueGreenDeploymentAndConfirm(client, deploymentIdentifier, blueclusterEndpoint, user_name, password)
+                if readOnly == "ON":
+                    logger.error("Aurora2: %s can't write, pls check", blueDBClusterIdentifier)
+                    return
+                # check cdc task's status is created
+                canStart = DMStaskStatInCreated(cdcArn, region)
+                if not canStart:
+                    logger.error("DMS replication task: %s for RDS/Aurora MySQL %s Created but not Started! Pls start it manually!" % (taskName, clusterId))
+                    return
+                # start cdc task
+                logger.info("Begin to start cdc task: %s", cdcArn)
+                startRes = dmsclient.start_replication_task(
+                    ReplicationTaskArn=cdcArn,
+                    StartReplicationTaskType='start-replication'
+                )
+                # check cdc status
+                repTask = startRes["ReplicationTask"] if startRes != None else None
+                taskStatus = repTask['Status'] if repTask != None else None
+                taskName = repTask['ReplicationTaskIdentifier'] if repTask != None else None
+                if taskStatus != "running":
+                    taskStatus = checkDMSCDCtaskStat(cdcArn, region)
+                    logger.warn("Status of task %s is %s." % (taskName, taskStatus))
+                    if taskStatus == "running":
+                        logger.info("SUCCESS: set DMS replication task: %s for RDS/Aurora MySQL %s succeeded" % (taskName, clusterId))
+                    else:
+                        logger.error("DMS replication task: %s for RDS/Aurora MySQL %s Created but not running" % (taskName, clusterId))
+            except Exception as e:
+                logger.error(e)
+                return
+        else:
+            logger.error("Source Endpoint connected: %s, Target Endpoint connected: %s, pls check the Security Groups and VPC of replication instance and DB instances", (testSrcResponse['Connection']['Status'], testDestResponse['Connection']['Status']))
+            return
+
     conn.close()
     logger.info("SUCCESS: set rollback replication for RDS/Aurora MySQL %s succeeded" % clusterId)
-    return "Added replication successfully for RDS/Aurora MySQL %s" % clusterId
+    return
